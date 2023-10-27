29932856c0589584143ddcc08dd845c934a8aede
Minister <mundestephane13@gmail.com>
2023-10-27 11:46:28 +0100

    Added lushify colorscheme

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+  -- Lush colorscheme for creating custom colors
+  { "rktjmp/lush.nvim" },
+
+  -- configure default colorscheme in lazyvim
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
