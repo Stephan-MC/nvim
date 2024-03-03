e900cc57ca880723a819e8027c52d5b2d46de437
Minister <mundestephane13@gmail.com>
2024-03-03 11:56:38 +0100

    Refactored the plugins directory to have a subdirectory for lsp configs

---
+++ lua/plugins/lsp/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {},
+      },
+      setup = {},
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      -- List of << Must Install >> servers
+      ensure_installed = {
+        "emmet-language-server",
+        "angular-language-server",
+        "tailwindcss-language-server",
+        "vue-language-server",
+        "intelephense",
+      },
+    },
+  },
+}
