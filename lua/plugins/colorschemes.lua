0c85526163342558f49bffe810c3ef59c0d9e939
Minister <mundestephane13@gmail.com>
2024-03-21 13:52:32 +0100

    Made neovim transparent

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,6 +2,17 @@ return {
   -- Lush colorscheme for creating custom colors
   { "rktjmp/lush.nvim" },
 
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
   -- configure default colorscheme in lazyvim
   {
     "LazyVim/LazyVim",
