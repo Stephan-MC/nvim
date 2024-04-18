5b0959619cfa3fe5e792869f869da86982f33ad2
Minister <mundestephane13@gmail.com>
2024-04-18 05:00:32 +0100

    fixed neorg to adapt to breaking changes

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -5,21 +5,28 @@ return {
     lazy = true,
     ft = "norg",
     cmd = "Neorg",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = function()
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/Documents/neorg-notes",
-              },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "vhyrro/luarocks.nvim", priority = 1000, opts = {} },
+      { "nvim-neorg/lua-utils.nvim", priority = 1000 },
+      { "pysan3/pathlib.nvim", priority = 1000 },
+    },
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              index = "~/Documents/neorg-notes",
             },
+            default_workspace = "index",
           },
         },
-      })
+      },
+    },
+    config = function(_, opts)
+      require("neorg").setup(opts)
     end,
   },
 }
