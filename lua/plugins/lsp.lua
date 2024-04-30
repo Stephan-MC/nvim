31a5b30214bbbe4510109bdfb801e9261133d8bf
Minister <mundestephane13@gmail.com>
2024-04-30 19:52:03 +0100

    Moved emmet lspconfig to lsp.lua to avoid neoconf.nvim error

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,7 +10,7 @@ return {
   -- },
 
   {
-    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
     opts = {
       -- List of << Must Install >> servers
       ensure_installed = {
@@ -23,5 +23,36 @@ return {
         "phpactor",
       },
     },
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig", "folke/neoconf.nvim" },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+
+      lspconfig.phpactor.setup({
+        init_options = {
+          ["language_server_phpstan.enabled"] = true,
+          ["language_server_phpstan.bin"] = vim.fn.stdpath("data") .. "/mason/bin/phpstan",
+          ["language_server_php_cs_fixer.enabled"] = true,
+          ["language_server_php_cs_fixer.show_diagnostics"] = false,
+          ["language_server_php_cs_fixer.bin"] = vim.fn.stdpath("data") .. "/mason/bin/php-cs-fixer",
+        },
+      })
+
+      lspconfig.emmet_language_server.setup({
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "pug",
+          "typescriptreact",
+          "blade",
+          "php",
+        },
+      })
+    end,
   },
 }
