03edddfb044f159eb30ca2b32ba28dc4972d5e4e
Minister <mundestephane13@gmail.com>
2024-06-17 03:50:52 +0100

    REmoved unnecessary lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,4 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     servers = {
-  --       tsserver = {},
-  --     },
-  --     setup = {},
-  --   },
-  -- },
-
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
@@ -24,17 +14,6 @@ return {
     },
     config = function(_, opts)
       local lspconfig = require("lspconfig")
-
-      -- lspconfig.phpactor.setup({
-      --   init_options = {
-      --     -- ["language_server_phpstan.enabled"] = true,
-      --     -- ["language_server_phpstan.bin"] = vim.fn.stdpath("data") .. "/mason/bin/phpstan",
-      --     ["language_server_php_cs_fixer.enabled"] = true,
-      --     ["language_server_php_cs_fixer.show_diagnostics"] = false,
-      --     ["language_server_php_cs_fixer.bin"] = vim.fn.stdpath("data") .. "/mason/bin/php-cs-fixer",
-      --     ["language_server_completion.trim_leading_dollar"] = true,
-      --   },
-      -- })
 
       lspconfig.emmet_language_server.setup({
         filetypes = {
